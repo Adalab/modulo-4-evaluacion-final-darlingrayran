864680594711db87d1341e69194b6dbd57e308a6
JohnathanEsguerra <darling.rayranc@gmail.com>
2023-11-14 23:24:04 +0100

    Add first endpoint to get all items from the database

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ async function getConnection(){
     const connection = await mysql.createConnection({
         host: "localhost",
         user: "root",
-        password:"",
+        password:"Jhonathan123",
         database:"colombianSongs",
     });
 
@@ -27,10 +27,35 @@ async function getConnection(){
     return connection;
 }
 
-
 //4. escuchar servidor
 
 const port = 4000;
 app.listen(port, ()=>{
     console.log(`servidor iniciado en http://localhost:${port}`);
 });
+
+//6. crear endpoints
+//6.1 Insertar una entrada en su entidad principal
+
+//6.2 Leer/listar todas las entradas existentes
+app.get("/allSongs", async(req, res)=>{
+    //select a la DB
+    let query = "SELECT * FROM colombianSongs.favoriteSongs";
+    //hacer conexion DB
+    const conn = await getConnection();
+    //ejecutar consulta
+    const [results] = await conn.query(query);
+    console.log(results);
+
+    //* longitud del array
+    const numOfElements = results.length;
+
+    //enviar una respuesta
+    res.json({
+        info: {count: numOfElements}, //numero de elementos que trae la lista*
+        results: results,
+    });
+
+});
+//6.3 Actualizar una entrada existente
+//6.4 Eliminar una entrada existente 
